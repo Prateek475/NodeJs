887f50f9a015a22beb96517d27e824079d7795bc
Prateek Dixit <prateekdixit252006@gmail.com>
2026-07-13 21:50:59 +0530

    aDDED THE functionality of user adding data and taking it to server and server routing

--- FirstNodeServer/first.js
+++ FirstNodeServer/first.js
@@ -1,17 +1,39 @@
 const http = require('http');
+const fs = require('fs');
 
 function requestListner(req,response) {
-  console.log(`uel : ${req.url},method : ${req.method},headers :${req.headers}`);
+  console.log(`url : ${req.url},method : ${req.method},headers :${req.headers}`);
   // process.exit();
-  response.setHeader('Content-type','text/html');
-  response.write('<html>');
-  response.write('<head><title>Complete Coding</title></head>');
   if(req.url == '/') {
+    response.setHeader('Content-type','text/html');
+    response.write('<html>');
+    response.write('<head><title>Complete Coding</title></head>');
+    response.write('<body>');
     response.write('<h1>Welcome to Complete Coding</h1>');
+    response.write('<h1>Enter your details</h1>');
+    response.write('<form action="/submit-form" method ="POST">');
+    response.write('<input type="text" placeholder="Enter your name" name ="username"> <br>');
+    response.write('<label for ="gender">Gender</label> <br>');
+    response.write('<label for ="male">Male</label>');
+    response.write('<input type="radio" name ="gender" value="male" id="male"/>');
+    response.write('<label for ="female">Female</label>');
+    response.write('<input type="radio" name ="gender" value="female" id="female"/>');
+    
+    response.write('<br><input type="submit" value="Submit"/>');
+    response.write('</form>');
   } else if(req.url.toLowerCase() == '/products') {
+    response.setHeader('Content-type','text/html');
+    response.write('<html>');
+    response.write('<head><title>Complete Coding</title></head>');
+    response.write('<body>');
     response.write('<h1>Products...</h1>');
+  } else if(req.url.toLowerCase() == '/submit-form' && req.method =='POST') {
+    fs.writeFileSync('user.txt','Prateek Dixit');
+    response.statusCode = 302;
+    response.setHeader('Location','/');
   }
-  response.write('<body><h1>LIKE / SHARE / SUBSCRIBE</h1></body>');
+  response.write('<h1>LIKE / SHARE / SUBSCRIBE</h1>');
+  response.write('</body>');
   response.write('</html>');
   response.end();
 }
